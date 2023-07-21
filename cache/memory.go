8fed04efa2e9e70b54adb6d326333b36fa170c05
zhiyin2021 <admin@47z.net>
2023-07-21 11:34:17 +0800

    调整缓存超时计算方式

--- cache/memory.go
+++ cache/memory.go
@@ -10,7 +10,7 @@ import (
 type item struct {
 	Value   any
 	Expired int64
-	Sliding int64
+	Sliding time.Duration
 }
 
 // NewMemory memory模式
@@ -35,7 +35,7 @@ func (m *Memory) Get(key string) any {
 		return nil
 	}
 	if item.Sliding > 0 {
-		now := time.Now().Unix()
+		now := time.Now().UnixMilli()
 		if (item.Expired - now) > int64(item.Sliding/2) {
 			m.mutex.RLock()
 			defer m.mutex.RUnlock()
@@ -52,7 +52,7 @@ func (m *Memory) getItem(key string) *item {
 		return nil
 	}
 	item := i.(*item)
-	if item.Expired < time.Now().Unix() {
+	if item.Expired < time.Now().UnixMilli() {
 		//过期
 		m.items.Delete(key)
 		//过期后删除
@@ -62,21 +62,19 @@ func (m *Memory) getItem(key string) *item {
 }
 
 // 获取缓存时自动延时
-func (m *Memory) SetBySliding(key string, val any, expire int64) {
-	sec := expire * int64(time.Second)
+func (m *Memory) SetBySliding(key string, val any, expire time.Duration) {
 	item := &item{
 		Value:   val,
-		Expired: time.Now().Unix() + sec,
-		Sliding: sec,
+		Expired: time.Now().UnixMilli() + expire.Milliseconds(),
+		Sliding: expire,
 	}
 	m.items.Store(key, item)
 }
 
-func (m *Memory) Set(key string, val any, expire int64) {
-	sec := expire * int64(time.Second)
+func (m *Memory) Set(key string, val any, expire time.Duration) {
 	item := &item{
 		Value:   val,
-		Expired: time.Now().Unix() + sec,
+		Expired: time.Now().UnixMilli() + expire.Milliseconds(),
 		Sliding: 0,
 	}
 	m.items.Store(key, item)
